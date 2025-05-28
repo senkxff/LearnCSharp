ced97ff6fb0fa6ea9b8073eeb5fd93205fb8e5fd
senkxff <xxzarenf@gmail.com>
2025-05-28 10:38:36 +0300

    Solved FirstTask

---
+++ ConditionalConstructions/ConditionalConstructions/FirstTask/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace FirstTask
+{
+    /*
+     * Напишите консольную программу, в которую пользователь вводит с клавиатуры два числа. 
+     * А программа сранивает два введенных числа и выводит на консоль результат сравнения 
+     * (два числа равны, первое число больше второго или первое число меньше второго).
+     */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваше первое число: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите Ваше второе число: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine("Первое число больше второго!");
+            }
+            else if (firstNumber < secondNumber)
+            {
+                Console.WriteLine("Первое число меньше второго!");
+            }
+            else
+            {
+                Console.WriteLine("Числа равны!");
+            }
+        }
+    }
+}
