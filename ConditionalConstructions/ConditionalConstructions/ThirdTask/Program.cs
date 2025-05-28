595121911c74aa2538bda2d146a04c3ba9d0b470
senkxff <xxzarenf@gmail.com>
2025-05-28 11:00:43 +0300

    Solved ThirdTask

---
+++ ConditionalConstructions/ConditionalConstructions/ThirdTask/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ThirdTask
+{
+    /*
+     * Напишите консольную программу, в которую пользователь вводит с клавиатуры число. Если число либо равно 5, либо равно 10, 
+     * то программа выводит "Число либо равно 5, либо равно 10". Иначе программа выводит сообщение "Неизвестное число".
+     */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите Ваше число: ");
+            double userNumber = Convert.ToDouble(Console.ReadLine());
+
+            if (userNumber == 5 || userNumber == 10)
+            {
+                Console.WriteLine("Число либо равно 5, либо равно 10");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестное число");
+            }
+        }
+    }
+}
