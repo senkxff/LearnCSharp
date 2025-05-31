d0ce3af72f334fe610818afe23f7112301a2afd3
senkxff <xxzarenf@gmail.com>
2025-05-31 13:54:31 +0300

    Solved ThirdTask

---
+++ Loops/Loops/ThirdTask/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ThirdTask
+{
+    /*
+     * Напишите программу, которая выводит на консоль таблицу умножения 
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Таблица умножения: ");
+
+            for (int firstNumber = 1; firstNumber <= 10; firstNumber++)
+            {
+                for (int secondNumber = 1; secondNumber <= 10; secondNumber++)
+                {
+                    Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}\t");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
