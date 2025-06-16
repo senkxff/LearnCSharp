d35718949cdad6845f5bacd09348b73d3210de41
senkxff <xxzarenf@gmail.com>
2025-06-16 08:40:42 +0300

    SimpleCsharpConsoleApp

---
+++ PLCShap/VSApplication/SimpleCsharpConsoleApp/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleCsharpConsoleApp
+{
+    public class Car
+    {
+        public int name
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
---
+++ PLCShap/VSApplication/SimpleCsharpConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleCsharpConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "My Rocking App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("****************************");
+            Console.WriteLine("********WELCOM!!!***********");
+            Console.WriteLine("****************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.ReadLine();
+        }
+    }
+}
